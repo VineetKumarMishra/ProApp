fd3e8f6c8011156cca328cd31bccd814dcaf0beb
Vineet Mishra <vineetmishea083@0utlook.com>
2024-12-04 09:02:25 +0530

    Set up initial Code

---
+++ ProApp/ProAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProAppApp.swift
+//  ProApp
+//
+//  Created by Vineet Mishra on 04/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ProAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProAppTests/ProAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProAppTests.swift
+//  ProAppTests
+//
+//  Created by Vineet Mishra on 04/12/24.
+//
+
+import Testing
+@testable import ProApp
+
+struct ProAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
