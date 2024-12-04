52fa302fe4ac464190d5ed67c40a86343f2050a7
Vineet Mishra <vineetmishea083@0utlook.com>
2024-12-04 15:13:22 +0530

    Design LoginView

---
+++ ProApp/DashboardView.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardView.swift
+//  ProApp
+//
+//  Created by Vineet Mishra on 04/12/24.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DashboardView()
+}
